56f965ea2c9b6ec500392e5aba936c901c75fe69
Bharath-Intrest <158258846+Bharath-Intrest@users.noreply.github.com>
2024-08-20 15:57:22 +0530

    Add files via upload

---
+++ Ai/beta/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . import model
+
+
+class betacls(APIView):
+    def post(self):
+        value = request.data.get('value')
+        model.test(value=value)
+        return Response("Hey Buddy")
